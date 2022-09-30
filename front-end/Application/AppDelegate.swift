bcc48d5f63445ad461068128ff13a9791a61c01d
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-09-30 20:00:29 +0900

    Dev/prod Setting

--- front-end/Application/AppDelegate.swift
+++ front-end/Application/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        #if DEBUG
+            print("DEBUG")
+        #elseif RELEASE
+            print("RELEASE")
+        #endif
         return true
     }
 
