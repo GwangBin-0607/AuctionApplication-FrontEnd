fd44b74108ca83f579082441d10e1c68f8fd9418
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-10-25 19:53:39 +0900

    First Commit

--- front-end/Application/ViewController.swift
+++ front-end/Application/ViewController.swift
@@ -6,19 +6,18 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
         guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "URLL") as? String else {
             fatalError("ApiKey must not be empty in plist")
         }
-        print(apiKey)
-        // Do any additional setup after loading the view.
     }
 
 
+}
+class t{
+    var name = "aaa"
 }
 
